c3b8bf7deeb3c01d425e1f806b6bac3fcdb6aabd
AdnanDevWeb <118213080+AdnanDevWeb@users.noreply.github.com>
2022-11-17 21:20:11 +0100

    playround

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 function getComputerChoice(){
     let choices = ["Rock","Paper" , "Scissors"]
     const randomChoice = Math.floor(Math.random()*choices.length)
-    console.log(choices[randomChoice])
-
+    return choices[randomChoice]
+}
+function getPlayerChoice(){
+    let choices = ["Rock","Paper" , "Scissors"]
+    const randomChoice = Math.floor(Math.random()*choices.length)
+    return choices[randomChoice]
 }
 getComputerChoice()
+getPlayerChoice()
